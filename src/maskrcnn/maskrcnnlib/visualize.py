ctu-geoforall-lab-projects/dp-pesek-2018

commit e340494c9854e97d2ab3c8879b600da268a6a2a6

follow fixes and updates of Waleed

src/maskrcnn/maskrcnnlib/visualize.py
@@ -8,6 +8,8 @@
 # WRITTEN:  	2017 Ondrej Pesek
 #                    Based on visualize.py by Waleed Abdulla (Matterport, Inc.)
 #                       https://github.com/matterport/Mask_RCNN
+#                       Written save_instances
+#                       Deleted display_differences, display_table
 # Licensed under the MIT License (see LICENSE for details)
 #
 #############################################################################
@@ -19,10 +21,8 @@ import os
 import numpy as np
 from skimage.measure import find_contours
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.lines as lines
+from matplotlib import patches,  lines
 from matplotlib.patches import Polygon
-# import IPython.display
 
 import utils
 
@@ -68,13 +68,13 @@ def random_colors(N, bright=True):
     return colors
 
 
-def apply_mask(image, mask, color):
+def apply_mask(image, mask, color, alpha=0.5):
     """Apply the given mask to the image.
     """
     for c in range(3):
         image[:, :, c] = np.where(mask == 1,
-                                  np.zeros([image.shape[0],
-                                            image.shape[1]]) + color[c],
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
                                   image[:, :, c])
     return image
 
@@ -90,6 +90,8 @@ def save_instances(image, boxes, masks, class_ids, class_names,
     class_names: list of class names of the dataset
     scores: (optional) confidence scores for each box
     figsize: (optional) the size of the image.
+
+    written by Ondrej Pesek
     """
 
     dpi = 80
@@ -448,20 +450,6 @@ def draw_boxes(image, boxes=None, refined_boxes=None,
     ax.imshow(masked_image.astype(np.uint8))
 
 
-# def display_table(table):
-#     """Display values in a table format.
-#     table: an iterable of rows, and each row is an iterable of values.
-#     """
-#     html = ""
-#     for row in table:
-#         row_html = ""
-#         for col in row:
-#             row_html += "<td>{:40}</td>".format(str(col))
-#         html += "<tr>" + row_html + "</tr>"
-#     html = "<table>" + html + "</table>"
-#     IPython.display.display(IPython.display.HTML(html))
-
-
 def display_weight_stats(model):
     """Scans all the weights in the model and returns a list of tuples
     that contain stats about each weight.
@@ -487,4 +475,3 @@ def display_weight_stats(model):
                 "{:+10.4f}".format(w.max()),
                 "{:+9.4f}".format(w.std()),
             ])
-    # display_table(table)

src/maskrcnn/ann.maskrcnn.train/ann.maskrcnn.train.py
@@ -31,6 +31,10 @@
 #%  key: b
 #%  description: Train also batch normalization layers (not recommended for small batches)
 #%end
+#%flag
+#%  key: n
+#%  description: No resizing or padding of images (images must be of the same size)
+#%end
 #%option G_OPT_M_DIR
 #% key: training_dataset
 #% label: Path to the dataset with images and masks

src/maskrcnn/maskrcnnlib/config.py
@@ -8,6 +8,8 @@
 # WRITTEN:  	2017    Ondrej Pesek
 #                       Based on config.py by Waleed Abdulla (Matterport, Inc.)
 #                           https://github.com/matterport/Mask_RCNN
+#                           Written __init__ to allow attributes to be
+#                               modified during the class call
 # Licensed under the MIT License (see LICENSE for details)
 #
 #############################################################################
@@ -43,7 +45,7 @@ class ModelConfig(object):
     RPN_ANCHOR_STRIDE = 1  # 2 before change
 
     # Non-max suppression threshold to filter RPN proposals.
-    # You can reduce this during training to generate more propsals.
+    # You can increase this during training to generate more proposals.
     RPN_NMS_THRESHOLD = 0.7
 
     # How many anchors per image to use for RPN training
@@ -53,8 +55,11 @@ class ModelConfig(object):
     POST_NMS_ROIS_TRAINING = 2000
     POST_NMS_ROIS_INFERENCE = 1000
 
-    # If True, pad images with zeros such that they're (max_dim by max_dim)
-    IMAGE_PADDING = True  # currently, the False option is not supported
+    # Minimum scaling ratio. Checked after MIN_IMAGE_DIM and can force further
+    # up scaling. For example, if set to 2 then images are scaled up to double
+    # the width and height, or more, even if MIN_IMAGE_DIM doesn't require it.
+    # Howver, in 'square' mode, it can be overruled by IMAGE_MAX_DIM.
+    IMAGE_MIN_SCALE = 0
 
     # Image mean (RGB)
     MEAN_PIXEL = np.array([123.7, 116.8, 103.9])
@@ -65,6 +70,9 @@ class ModelConfig(object):
     # Pooled ROIs
     POOL_SIZE = 7
     MASK_POOL_SIZE = 14
+
+    # Shape of output mask
+    # To change this you also need to change the neural network mask branch
     MASK_SHAPE = [28, 28]
 
     # Maximum number of ground truth instances to use in one image
@@ -94,6 +102,16 @@ class ModelConfig(object):
     # Weight decay regularization
     WEIGHT_DECAY = 0.0001
 
+    # Loss weights for more precise optimization.
+    # Can be used for R-CNN training setup.
+    LOSS_WEIGHTS = {
+        "rpn_class_loss": 1.,
+        "rpn_bbox_loss": 1.,
+        "mrcnn_class_loss": 1.,
+        "mrcnn_bbox_loss": 1.,
+        "mrcnn_mask_loss": 1.
+    }
+
     # Use RPN ROIs or externally generated ROIs for training
     # Keep this True for most situations. Set to False if you want to train
     # the head branches on ROI generated by code rather than the ROIs from
@@ -101,18 +119,21 @@ class ModelConfig(object):
     # train the RPN.
     USE_RPN_ROIS = True
 
+    # Gradient norm clipping
+    GRADIENT_CLIP_NORM = 5.0
+
     def __init__(self, name='model', imagesPerGPU=1, GPUcount=1, numClasses=1,
                  trainROIsPerImage=64, stepsPerEpoch=1500,
                  miniMaskShape=None, validationSteps=100,
                  imageMaxDim=768, imageMinDim=768, backbone='resnet101',
-                 trainBatchNorm=False):
+                 trainBatchNorm=False, resizeMode='square'):
         """Set values of attributes.
-        Written by Ondrej Pesek"""
+        Written by Ondrej Pesek, but using attributes from Waleed Abdulla"""
 
         # Give the configuration a recognizable name
         self.NAME = name
 
-        # Number of images per GPU
+        # Number of images to train on each GPU
         self.IMAGES_PER_GPU = imagesPerGPU
 
         # NUMBER OF GPUs to use. For CPU training, use 1
@@ -120,26 +141,46 @@ class ModelConfig(object):
 
         # Number of classes (including background)
         self.NUM_CLASSES = numClasses
+
+        # Number of ROIs per image to feed to classifier/mask heads
+        # Try to keep keep a positive:negative
+        # ratio of 1:3. You can increase the number of proposals by adjusting
+        # the RPN NMS threshold.
         self.TRAIN_ROIS_PER_IMAGE = trainROIsPerImage
+
+        # Number of training steps per epoch. A model is saved after each epoch
         self.STEPS_PER_EPOCH = stepsPerEpoch // self.IMAGES_PER_GPU
+
+        # Number of steps to run at the end of every training epoch
         self.VALIDATION_STEPS = validationSteps
+
+        # Input image resizing
+        # Generally, use the "square" resizing mode for training and inferencing
+        # and it should work well in most cases. In this mode, images are scaled
+        # up such that the small side is = IMAGE_MIN_DIM, but ensuring that the
+        # scaling doesn't make the long side > IMAGE_MAX_DIM. Then the image is
+        # padded with zeros to make it a square so multiple images can be put
+        # in one batch.
+        # Available resizing modes:
+        # none:   No resizing or padding. Return the image unchanged.
+        # square: Resize and pad with zeros to get a square image
+        #         of size [max_dim, max_dim].
+        self.IMAGE_RESIZE_MODE = resizeMode
         self.IMAGE_MAX_DIM = imageMaxDim
         self.IMAGE_MIN_DIM = imageMinDim
 
         # mini_mask to save memory
+        # If True, resizes masks to a smaller size to reduce memory load
         if miniMaskShape:
             self.USE_MINI_MASK = True
-            self.MINI_MASK_SHAPE = tuple(int(a) for a in miniMaskShape.split(','))
+            self.MINI_MASK_SHAPE = tuple(
+                int(a) for a in miniMaskShape.split(','))  # (height, width)
         else:
             self.USE_MINI_MASK = False
             self.MINI_MASK_SHAPE = None
 
         # Effective batch size
         self.BATCH_SIZE = self.IMAGES_PER_GPU * self.GPU_COUNT
-
-        # Input image size
-        self.IMAGE_SHAPE = np.array(
-            [self.IMAGE_MAX_DIM, self.IMAGE_MAX_DIM, 3])
 
         # Set the backbone architecture
         self.BACKBONE = backbone
@@ -155,6 +196,14 @@ class ModelConfig(object):
         #  False: Freeze BN layers (recommended for small batch size)
         #  True: Set layer in training mode even when inferencing
         self.TRAIN_BN = trainBatchNorm
+
+        # Input image size
+        self.IMAGE_SHAPE = np.array(
+            [self.IMAGE_MAX_DIM, self.IMAGE_MAX_DIM, 3])
+
+        # Image meta data length
+        # See compose_image_meta() for details
+        self.IMAGE_META_SIZE = 1 + 3 + 3 + 4 + 1 + self.NUM_CLASSES
 
     def display(self):
         """Display Configuration values."""

src/maskrcnn/maskrcnnlib/parallel_model.py
@@ -89,16 +89,18 @@ class ParallelModel(KM.Model):
         with tf.device('/cpu:0'):
             merged = []
             for outputs, name in zip(outputs_all, output_names):
-                # If outputs are numbers without dimensions, add a batch dim.
-                def add_dim(tensor):
-                    """Add a dimension to tensors that don't have any."""
-                    if K.int_shape(tensor) == ():
-                        return KL.Lambda(lambda t: K.reshape(t, [1, 1]))(tensor)
-                    return tensor
-                outputs = list(map(add_dim, outputs))
-
-                # Concatenate
-                merged.append(KL.Concatenate(axis=0, name=name)(outputs))
+                # Concatenate or average outputs?
+                # Outputs usually have a batch dimension and we concatenate
+                # across it. If they don't, then the output is likely a loss
+                # or a metric value that gets averaged across the batch.
+                # Keras expects losses and metrics to be scalars.
+                if K.int_shape(outputs[0]) == ():
+                    # Average
+                    m = KL.Lambda(lambda o: tf.add_n(o) / len(outputs), name=name)(outputs)
+                else:
+                    # Concatenate
+                    m = KL.Concatenate(axis=0, name=name)(outputs)
+                merged.append(m)
         return merged
 
 
@@ -118,10 +120,10 @@ if __name__ == "__main__":
     GPU_COUNT = 2
 
     # Root directory of the project
-    ROOT_DIR = os.getcwd()
+    ROOT_DIR = os.path.abspath("../")
 
     # Directory to save logs and trained model
-    MODEL_DIR = os.path.join(ROOT_DIR, "logs/parallel")
+    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
     def build_model(x_train, num_classes):
         # Reset default graph. Keras leaves old ops in the graph,

src/maskrcnn/maskrcnnlib/py3train.py
@@ -36,6 +36,13 @@ def train(dataset, modelPath, classes, logs, modelName, imagesPerGPU=1,
         # None means train in normal mode but do not force it when inferencing
         trainBatchNorm = None
 
+    if 'n' not in flags:
+        # Resize and pad with zeros to get a square image of
+        # size [max_dim, max_dim].
+        resizeMode = 'square'
+    else:
+        resizeMode = 'none'
+
     # Configurations
     config = ModelConfig(name=modelName,
                          imagesPerGPU=imagesPerGPU,
@@ -48,10 +55,9 @@ def train(dataset, modelPath, classes, logs, modelName, imagesPerGPU=1,
                          imageMaxDim=imMaxDim,
                          imageMinDim=imMinDim,
                          backbone=backbone,
-                         trainBatchNorm=trainBatchNorm)
+                         trainBatchNorm=trainBatchNorm,
+                         resizeMode=resizeMode)
     config.display()
-
-    # raise SystemExit(0)
 
     # Create model
     model = modellib.MaskRCNN(mode="training", config=config,
@@ -89,16 +95,18 @@ def train(dataset, modelPath, classes, logs, modelName, imagesPerGPU=1,
 
     evalImagesThreshold = int(testImagesThreshold * .75)
 
+    # augmentation = imgaug/augmenters.Fliplr(0.5)
+
     # Training dataset
     trainImages = images[:evalImagesThreshold]
     dataset_train = utils.Dataset()
-    dataset_train.import_contains(classes, trainImages, modelName)
+    dataset_train.import_contents(classes, trainImages, modelName)
     dataset_train.prepare()
 
     # Validation dataset
     evalImages = images[evalImagesThreshold:testImagesThreshold]
     dataset_val = utils.Dataset()
-    dataset_val.import_contains(classes, evalImages, modelName)
+    dataset_val.import_contents(classes, evalImages, modelName)
     dataset_val.prepare()
 
     # Training - Stage 1
@@ -107,7 +115,7 @@ def train(dataset, modelPath, classes, logs, modelName, imagesPerGPU=1,
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=int(epochs / 7),
-                layers='heads')
+                layers='heads')  # augmentation=augmentation
 
     # Training - Stage 2
     # Finetune layers from ResNet stage 4 and up
@@ -115,7 +123,7 @@ def train(dataset, modelPath, classes, logs, modelName, imagesPerGPU=1,
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE / 10,  # no dividing orig
                 epochs=int(epochs / 7) * 3,
-                layers='4+')
+                layers='4+')  # augmentation=augmentation
 
     # Training - Stage 3
     # Fine tune all layers
@@ -123,7 +131,7 @@ def train(dataset, modelPath, classes, logs, modelName, imagesPerGPU=1,
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE / 100,  # just 10 original
                 epochs=epochs,
-                layers='all')
+                layers='all')  # augmentation=augmentation
 
 if __name__ == '__main__':
 

commit aa91dc45d139a89dc179150a4e34ca72355f830d

fix errors in saving images introduced in e340494

src/maskrcnn/maskrcnnlib/visualize.py
@@ -8,8 +8,9 @@
 # WRITTEN:  	2017 Ondrej Pesek
 #                    Based on visualize.py by Waleed Abdulla (Matterport, Inc.)
 #                       https://github.com/matterport/Mask_RCNN
-#                       Written save_instances
-#                       Deleted display_differences, display_table
+#                       Written save_instances, apply_mask
+#                       Deleted display_differences, display_table,
+#                           display_images, display_top_masks
 # Licensed under the MIT License (see LICENSE for details)
 #
 #############################################################################
@@ -31,29 +32,6 @@ import utils
 #  Visualization
 ############################################################
 
-def display_images(images, titles=None, cols=4, cmap=None, norm=None,
-                   interpolation=None):
-    """Display the given set of images, optionally with titles.
-    images: list or array of image tensors in HWC format.
-    titles: optional. A list of titles to display with each image.
-    cols: number of images per row
-    cmap: Optional. Color map to use. For example, "Blues".
-    norm: Optional. A Normalize instance to map values to colors.
-    interpolation: Optional. Image interporlation to use for display.
-    """
-    titles = titles if titles is not None else [""] * len(images)
-    rows = len(images) // cols + 1
-    plt.figure(figsize=(14, 14 * rows // cols))
-    i = 1
-    for image, title in zip(images, titles):
-        plt.subplot(rows, cols, i)
-        plt.title(title, fontsize=9)
-        plt.axis('off')
-        plt.imshow(image.astype(np.uint8), cmap=cmap,
-                   norm=norm, interpolation=interpolation)
-        i += 1
-    plt.show()
-
 
 def random_colors(N, bright=True):
     """
@@ -68,13 +46,15 @@ def random_colors(N, bright=True):
     return colors
 
 
-def apply_mask(image, mask, color, alpha=0.5):
+def apply_mask(image, mask, colour):
     """Apply the given mask to the image.
+
+    Written by Ondrej Pesek
     """
     for c in range(3):
         image[:, :, c] = np.where(mask == 1,
-                                  image[:, :, c] *
-                                  (1 - alpha) + alpha * color[c] * 255,
+                                  np.zeros([image.shape[0],
+                                            image.shape[1]]) + colour[c],
                                   image[:, :, c])
     return image
 
@@ -91,7 +71,7 @@ def save_instances(image, boxes, masks, class_ids, class_names,
     scores: (optional) confidence scores for each box
     figsize: (optional) the size of the image.
 
-    written by Ondrej Pesek
+    Written by Ondrej Pesek
     """
 
     dpi = 80
@@ -268,29 +248,6 @@ def draw_box(image, box, color):
     image[y1:y2, x1:x1 + 2] = color
     image[y1:y2, x2:x2 + 2] = color
     return image
-
-
-def display_top_masks(image, mask, class_ids, class_names, limit=4):
-    """Display the given image and the top few class masks."""
-    to_display = []
-    titles = []
-    to_display.append(image)
-    titles.append("H x W={}x{}".format(image.shape[0], image.shape[1]))
-    # Pick top prominent classes in this image
-    unique_class_ids = np.unique(class_ids)
-    mask_area = [np.sum(mask[:, :, np.where(class_ids == i)[0]])
-                 for i in unique_class_ids]
-    top_ids = [v[0] for v in sorted(zip(unique_class_ids, mask_area),
-                                    key=lambda r: r[1], reverse=True) if v[1] > 0]
-    # Generate images and titles
-    for i in range(limit):
-        class_id = top_ids[i] if i < len(top_ids) else -1
-        # Pull masks of instances belonging to the same class.
-        m = mask[:, :, np.where(class_ids == class_id)[0]]
-        m = np.sum(m * np.arange(1, m.shape[-1] + 1), -1)
-        to_display.append(m)
-        titles.append(class_names[class_id] if class_id != -1 else "-")
-    display_images(to_display, titles=titles, cols=limit + 1, cmap="Blues_r")
 
 
 def plot_precision_recall(AP, precisions, recalls):